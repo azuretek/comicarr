07e00926afad68c6518478f016cb06832e50590c
Frankie Ramirez <915310+frankieramirez@users.noreply.github.com>
2026-07-16 09:55:04 -0400

    fix(review): add required test license headers

--- tests/unit/test_manga_import_regressions.py
+++ tests/unit/test_manga_import_regressions.py
@@ -1,3 +1,12 @@
+#  Copyright (C) 2026 Comicarr contributors
+#
+#  This file is part of Comicarr.
+#
+#  Comicarr is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
 """Regression coverage for manga import failures reported in issue #278."""
 
 from unittest.mock import MagicMock, patch
